53f0dc0cec7c4c74a31248c8b4ac263a3f49b912
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-06-01 02:49:28 +0000

    fixed view error.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- application/views/projects/thankyou.php
+++ application/views/projects/thankyou.php
@@ -1,8 +1,4 @@
-<?php $this->load->view('header'); ?>
-<!-- Candy comes in a wrapper -->
 <div id="wrapperWit">
-    <!-- Hard candy outer shell -->
-    <!-- Gooey center -->
     <div id="gooey">
         <div id="content">
             <h2>CodeIgniter 應用案例</h2>
@@ -14,13 +10,12 @@
                     <p>你的網站還沒有列在這嗎？趕快<a href="<?=site_url('projects/submit');?>">提交</a>到 CodeIgniter 應用案例列表中！</p>
                 </div>
                 <ul class="ci_sites">
-        					<?php foreach($ProjectsList as $item):?>
-        					<li><a href="<?=$item['project_url'];?>"><?=$item['project_name'];?></a></li>
-        					<?php endforeach;?>
+                            <?php foreach($ProjectsList as $item):?>
+                            <li><a href="<?=$item['project_url'];?>"><?=$item['project_name'];?></a></li>
+                            <?php endforeach;?>
                 </ul>
             </div>
             <div class="clear"></div>
         </div>
     </div>
 </div>
-<?php $this->load->view('footer'); ?>
